49525d8323d62b1eb483039c64c0c260df1a8dc1
new-slemme <68908036+new-slemme@users.noreply.github.com>
2026-02-16 18:19:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ class Player(db.Model):
 class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    retired = db.Column(db.Boolean, nullable=False, default=False)
 
     # legacy / user-entered fallback
     commander = db.Column(db.String(100), nullable=False)
